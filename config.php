f5fcb241452f2db70c53ed78f6b5071063f5ba50
phoenix9469 <82319443+phoenix9469@users.noreply.github.com>
2022-12-07 19:55:49 +0900

    update

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 	define("DB_PASSWORD", "1q2w3e4r");
 	define("DB_NAME", "OSJ");
 
-	define("GOOGLE_API_KEY", "AAAAK-FImpw:APA91bFoP9EqeG89rNlPn9BBKMDxw9u1HARrBk6AQHFzvIq4bY4ee92o8rlHYGae-d95nmtdbGY2arwEHrWAZEFSKlYkHcxsZfeQdKrqsaIJRj85dzBwEXOvtYPYtzA27Z507VcIZh14"); 
+	define("GOOGLE_API_KEY", ""); 
 
 ?>
